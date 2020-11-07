cf09477b32f1a0de645f0bc0ae6752a503febe7e
Alex Moon <alex.moon@printed.com>
2020-11-07 16:37:39 +0000

    VOS-11 - let's revert

--- run.sh
+++ run.sh
@@ -3,13 +3,5 @@ green="$(tput setaf 2)"
 reset="$(tput sgr0)"
 function green() { echo "${green}${@}${reset}"; }
 
-cert=/etc/letsencrypt/live/vos.ajmoon.uk/fullchain.pem
-export PATH="/home/ubuntu/.config/composer/vendor/bin:$PATH"
-
 green 'Visit http://localhost:8000/sun/1/m in your browser'
-if [[ -f "$cert" ]]; then
-    hyper-run -S 0.0.0.0:8000  -s 0.0.0.0:443 -r router.php -c $cert
-else
-    hyper-run -S 0.0.0.0:8000 -s 0.0.0.0:443 -r router.php
-fi
-
+php -S 0.0.0.0:8000 router.php
