68bca47f0229a1d936c9a579ec0170eac733b408
Alex Moon <alex.moon@printed.com>
2020-11-07 12:48:06 +0000

    VOS-11 - SSL with letsencrypt...

--- run.sh
+++ run.sh
@@ -3,7 +3,13 @@ green="$(tput setaf 2)"
 reset="$(tput sgr0)"
 function green() { echo "${green}${@}${reset}"; }
 
+cert=/etc/letsencrypt/live/vos.ajmoon.uk/fullchain.pem
+
 green 'Visit http://localhost:8000/sun/1/m in your browser'
 # php -S 0.0.0.0:8000 router.php
-hyper-run -S 0.0.0.0:8000 -s 0.0.0.0:443 router.php
+if [[ -f "$cert" ]]; then
+    hyper-run -S 0.0.0.0:8000  -s 0.0.0.0:443 -r router.php -c $cert
+else
+    hyper-run -S 0.0.0.0:8000 -s 0.0.0.0:443 -r router.php
+fi
 
