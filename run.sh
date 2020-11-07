ae9ede19113533cfd865c0dbcf715c2006136364
Alex Moon <alex.moon@printed.com>
2020-11-07 12:37:29 +0000

    VOS-11 - SSL (easiest way I could find)

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ reset="$(tput sgr0)"
 function green() { echo "${green}${@}${reset}"; }
 
 green 'Visit http://localhost:8000/sun/1/m in your browser'
-php -S 0.0.0.0:8000 router.php
+# php -S 0.0.0.0:8000 router.php
+hyper-run -S 0.0.0.0:8000 -s 0.0.0.0:443 router.php
+
