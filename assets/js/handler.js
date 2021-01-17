bf06bb5321814309a4ec6272f6897323509a5b86
Alex Moon <alex.moon@printed.com>
2021-01-17 14:22:01 +0000

    VOS-10 - fudge for mobile long press
    https://stackoverflow.com/questions/43459539/mapbox-gl-js-long-tap-press

--- assets/js/handler.js
+++ assets/js/handler.js
@@ -4,6 +4,26 @@ function Handler(map, view) {
     this.view = view;
     this.map.on('click', this.click.bind(this));
     this.map.on('contextmenu', this.rightClick.bind(this));
+
+    let mobileTimeout;
+    let clearMobileTimeout = () => { clearTimeout(mobileTimeout); };
+    this.map.on('touchstart', (e) => {
+        if (e.originalEvent.touches.length <= 1) {
+            mobileTimeout = setTimeout(
+                this.rightClick.bind(this),
+                500
+            );
+        }
+    });
+    this.map.on('touchend', clearMobileTimeout);
+    this.map.on('touchcancel', clearMobileTimeout);
+    this.map.on('touchmove', clearMobileTimeout);
+    this.map.on('pointerdrag', clearMobileTimeout);
+    this.map.on('pointermove', clearMobileTimeout);
+    this.map.on('moveend', clearMobileTimeout);
+    this.map.on('gesturestart', clearMobileTimeout);
+    this.map.on('gesturechange', clearMobileTimeout);
+    this.map.on('gestureend', clearMobileTimeout);
 }
 Object.assign(Handler.prototype, {
     click(event) {
